1a460d3dc4f5b9bca87daab02c09242802a405a3
RamSangineni <118288906+RamSangineni@users.noreply.github.com>
2023-03-05 21:12:51 +0530

    Update deploy.py

--- deploy.py
+++ deploy.py
@@ -73,3 +73,11 @@ def main():
 
 if __name__=='__main__':
    main()
+
+
+uploaded_file = st.file_uploader(
+    "Choose your database", accept_multiple_files=False)
+if uploaded_file is not None:
+    file_name = uploaded_file
+else:
+    file_name = "DatabaseSample.xlsx"
